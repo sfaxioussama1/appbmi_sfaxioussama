4e0508528e3edcf7d854e601cacc9fda6841650c
sfaxioussama1 <sf.dev.oussama3@gmail.com>
2020-11-28 13:52:27 +0100

    home icon male done
    home icon male done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -55,7 +55,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                     Text('MALE', style: TextStyle(
                       fontSize: 18.0,
-                      color: Colors.grey
+                     
 
                     ))
                   ],
